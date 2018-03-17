61687efa76b50ec5a54add9829839728a56c7271
nsch1 <nick.schooneman1@gmail.com>
2018-03-17 14:08:22 +0100

    Some refactoring & add check for duplicate items

--- index.js
+++ index.js
@@ -6,34 +6,59 @@ const mup = new Meetup()
 
 const topicsCounter = {}
 
+let lastTenItems = []
+
 server.listen(3002)
+
+const countOccurences = (topics) => {
+  topics.forEach(name => {
+    topicsCounter[name] ? topicsCounter[name]++ : topicsCounter[name] = 1
+  })
+}
+
+const topTen = () => {
+  return Object.keys(topicsCounter)
+    .sort((topicA, topicB) => topicsCounter[topicB] - topicsCounter[topicA])
+    .slice(0, 10)
+    .map(topic => {
+      return { topic, count: topicsCounter[topic] }
+    })
+}
+
+const inLastTenItems = (item) => {
+  return lastTenItems.filter(i => {
+    return i[0] === item.member.member_id &&
+    i[1] === item.event.event_id
+  }).length > 0
+}
+
+const addToLastTenItems = (item) => {
+  return [[item.member.member_id, item.event.event_id], ...lastTenItems].slice(0, 10)
+}
 
 mup.stream("/2/rsvps", stream => {
   stream
     .on("data", item => {
       const topicNames = item.group.group_topics.map(topic => topic.topic_name)
-      if (topicNames.includes('Software Development')) {
-        topicNames.forEach(name => {
-          if (topicsCounter[name]) {
-            topicsCounter[name]++
-          }
-          else {
-            topicsCounter[name] = 1
-          }
-        })
-        const arrayOfTopics = Object.keys(topicsCounter)
 
-        arrayOfTopics.sort((topicA, topicB) => {
-          return topicsCounter[topicB] - topicsCounter[topicA]
-        })
+      if (!topicNames.includes('Software Development')) return
 
-        const topTopics = arrayOfTopics.slice(0, 10).map(topic => {
-          return { topic, count: topicsCounter[topic] }
-        })
+      if (inLastTenItems(item)) return
 
-        io.emit('action', { topTopics })
+      lastTenItems = addToLastTenItems(item)
 
-      }
+      countOccurences(topicNames)
+
+      io.emit('action', {
+        type: 'UPDATE_TOPICS',
+        payload: topTen()
+      })
+
+      io.emit('action', {
+        type: 'ADD_RSVP',
+        payload: item
+      })
+
     }).on("error", e => {
        console.log("error! " + e)
     })
@@ -41,4 +66,8 @@ mup.stream("/2/rsvps", stream => {
 
 io.on('connection', socket => {
   console.log('got connection')
+  socket.emit('action', {
+    type: 'UPDATE_TOPICS',
+    payload: topTen()
+  })
 })
